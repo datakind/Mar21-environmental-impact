728e2eb3b9d727b62bd6be491b7d52a155f88dff
Ashish Dutt <ashish.dutt8@gmail.com>
2021-03-05 21:20:06 +0800

    modified script
    - determine variables with missing values

--- scripts/_duttashi/national_acs5-2018_census_and_brownfields_data_exploratory_analysis.R
+++ scripts/_duttashi/national_acs5-2018_census_and_brownfields_data_exploratory_analysis.R
@@ -11,11 +11,11 @@ rm(list = ls())
 
 # required libraries
 library(tidyverse) # for data manipulation
-library(data.table) # for setnames()
+
 
 # read the raw dataset in memory
-df_raw_census <- read.csv("data/national_acs5-2018_census.csv", na.strings = NA)
-df_raw_brownfields <- read.csv("data/brownfields_data_with_county_geoid.csv", na.strings = NA)
+df_raw_census <- read.csv("data/national_acs5-2018_census.csv", na.strings=c("","NA"))
+df_raw_brownfields <- read.csv("data/brownfields_data_with_county_geoid.csv", na.strings=c("","NA"))
 
 # 1. Exploratory Data Analysis
 
@@ -25,8 +25,8 @@ dim(df_raw_brownfields) # [1] 78527 rows in 150 columns
 
 # 1.2. check for missing data
 sum(is.na(df_raw_census)) # 237712 missing values
-sum(is.na(df_raw_brownfields)) # 1205125  missing values
-colSums(is.na(df_raw_census))
+sum(is.na(df_raw_brownfields)) # 7517972  missing values
+
 
 # 1.3 Find variables with zero variance and remove them
 excluded_vars  <- df_raw_census %>%
@@ -36,35 +36,24 @@ excluded_vars  <- df_raw_census %>%
 ## Finding: df_raw_census data has 49 variables with zero variance 
 ## Finding: df_raw_brownfields data has 01 variable with zero variance
 
-# remove the 49 variables with zero variance
+# remove the 49 variables with zero variance from df_raw_census dataframe
 df_raw_census<- df_raw_census %>%
   select(-one_of(excluded_vars))
 dim(df_raw_census) # [1] 3220  985
 
+# remove the 01 variable with zero variance from df_raw_brownfields dataframe
+excluded_vars  <- df_raw_brownfields %>%
+  summarise_all(var) %>%
+  select_if(function(.) . == 0) %>% 
+  names()
+df_raw_brownfields <- df_raw_brownfields %>%
+  select(-one_of(excluded_vars))
+dim(df_raw_brownfields) # [1] 78527   149
+
 # remove missing data from df_raw_census dataframe
 df_raw_census <- df_raw_census %>%
   drop_na()
-dim(df_raw_census) # [1] 3142  985
 
-# 1.4 Lowercase column names
-lowercase_cols<- function(df){
-  for (col in colnames(df)) {
-    colnames(df)[which(colnames(df)==col)] = tolower(col)
-  }
-  return(df)
-}
-# lower case all variable names
-df_raw_census <- lowercase_cols(df_raw_census)
-df_raw_brownfields <- lowercase_cols(df_raw_brownfields)
-
-# I find the variable GEOCODE is common in both df_raw_census and df_raw_brownfields dataframes.
-# However, GEOID in census needs cleaning. It has an additional 0 prefix to each geoid that needs to be removed
-
-df_raw_census$geoid<- as.character(df_raw_census$geoid)
-df_raw_brownfields$geoid<- as.character(df_raw_brownfields$geoid)
-# So I'll now join both these files on geoid
-df_raw_brownfields_census <- inner_join(df_raw_brownfields, df_raw_census, by=c("geoid"))
-dim(df_raw_brownfields_census) # 77856 rows in 1134 columns
-
-# write the raw joined file to disc
-write.csv(df_raw_brownfields_census, file =  "data//_volunteer_created_datasets//df_raw_brownfields_census.csv")
+colSums(is.na(df_raw_brownfields))
+names(df_raw_brownfields)[colSums(is.na(df_raw_brownfields)) >0] 
+# # 143 variables out of  149 variables in brownfields dataframe has missing values
