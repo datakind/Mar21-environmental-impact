11820dcbed655d79aec64b4ba3728fc97f60d9f8
Shanti Agung <shanti.agung@gmail.com>
2021-03-05 15:22:47 -0500

    apply david's cleanup.R

--- scripts/shanti-agung/toclean_browfield_data.r
+++ scripts/shanti-agung/toclean_browfield_data.r
@@ -86,17 +86,21 @@ brownfields %>%
 brownfields %>% 
   count(`Cntmnt Fnd-Other Metals`) # possibly need to recode "x"
 
-# david'sfunction 
+#### use david's function to cleanup ####
+# Common Functions
 move_digits_to_back <- function(x) {
   if (grepl("^[0-9]{4}", x)) {
     split_var <- strsplit(x, split = "_")[[1]]
     paste(c(split_var[2:length(split_var)], split_var[1]), collapse = "_")
   } else x
 }
+
 format_cols <- function(x) {
+  
   make_title_case <- function(x) {
     ifelse(x == toupper(x), str_to_title(x), x)
   }
+  
   x <- x %>% 
     make_title_case() %>% 
     str_replace_all('\\%', 'Pct') %>% 
@@ -110,34 +114,82 @@ format_cols <- function(x) {
   x
 }
 
-# implement david's function
-brownfields_new <- brownfields %>% select(where(~!all(is.na(.x))))
-colnames(brownfields_new) <- sapply(colnames(brownfields_new), move_digits_to_back)
-clean_columns <- brownfields_new %>% rename_all(format_cols)
-colnames(clean_columns)
-
-# fix var content
-clean_columns %>% 
-  mutate(Cntmnt_Fnd_Other_Metals = case_when(Cntmnt_Fnd_Other_Metals == "x" ~ "Y",
-                                             TRUE ~ Cntmnt_Fnd_Other_Metals)) %>% 
-  count(Cntmnt_Fnd_Other_Metals)
-
-fnclean_cntmnt <- function(cntmnt){
-  mutate(cntmnt = case_when(cntmnt == "x" ~ "Y",
-                                             TRUE ~ cntmnt))
+clean_city_property <- function(x) {
+  gsub("[0-9]{5}", "", x) %>%
+    gsub("[[:space:]]+$", "", .) %>%
+    gsub("[[:punct:]]$", "", .) %>%
+    gsub(",[A-Z]{2}$", "", .) %>%
+    toupper
 }
 
-clean_columns %>% 
-  mutate_at(c("Cntmnt_Fnd_Other_Metals", "Cntmnt_Fnd_Petroleum"), fnclean_cntmnt)
+count_cntmtn_fnd <- function(x) {
+  x %>% 
+    select(contains('Cntmnt_Fnd')) %>% 
+    mutate(across(contains('Cntmnt_Fnd'), ~ ifelse(.x == 'Y', 1, 0))) %>% 
+    rowSums(na.rm = TRUE)
+}
 
-clean_columns %>% 
-  mutate(across(c("Cntmnt_Fnd_Other_Metals", "Cntmnt_Fnd_Petroleum"), fnclean_cntmnt)) %>% 
-  count(Cntmnt_Fnd_Other_Metals)
 
-clean_columns %>% 
-  mutate(Cntmnt_Fnd_Other_Metals = case_when(Cntmnt_Fnd_Other_Metals == "x" ~ "Y",
-                                             TRUE ~ Cntmnt_Fnd_Other_Metals)) %>% 
-  mutate(Cntmnt_Fnd_Petroleum = case_when(Cntmnt_Fnd_Petroleum == "x" ~ "Y",
-                                             TRUE ~ Cntmnt_Fnd_Petroleum)) 
-  
+# implement david's function
+# brownfields_new <- brownfields %>% select(where(~!all(is.na(.x))))
+# colnames(brownfields_new) <- sapply(colnames(brownfields_new), move_digits_to_back)
+# clean_columns <- brownfields_new %>% rename_all(format_cols)
+# colnames(clean_columns)
+
+# Remove All Missing and Unnecessary Columns
+geo_data <- brownfields %>% 
+  select(where(~!all(is.na(.x)))) %>% 
+  select(-`Future Use: Multistory (arces)`)
+
+# Rename and Format Columns
+geo_data <- geo_data %>% rename_all(format_cols) %>% rename(GEOID = Geoid)
+colnames(geo_data) <- sapply(colnames(geo_data), move_digits_to_back)
+
+# Apply Cleaning to individual values
+geo_clean <- geo_data %>% 
+  mutate(across(contains('Date') & contains('-'), as.Date, format = '%Y-%d-%m')) %>% # I don't think this does what I think it does - is it really checking for a hyphen?
+  mutate_at(vars(Date_ICs_in_Place, Redev_Completion_Date), as.Date, format = '%m/%d/%y') %>% # A few dates are not in the standard format
+  mutate_if(~ length(unique(levels(as.factor(.x)))) < 100, as.factor) %>% # Convert shorter character values to factors
+  mutate_if(is.factor, ~ .x %>% fct_recode(Y = 'x', Y = 'y',N = 'n', U = 'u')) %>% # Recode those factors that don't match. 
+  mutate(Property_City_clean = clean_city_property(Property_City)) %>%
+  mutate_at(vars(GEOID), as.numeric) %>% 
+  mutate(Num_Cntmtn_Fnd = count_cntmtn_fnd(.))
+
+
+# ACS Data Cleaning -------------------------------------------------------
+
+
+acs_data_raw <- readxl::read_xlsx('data/national_acs5-2018_census.xlsx')
+
+# Ensure joining between ACS and GEO
+acs_data <- acs_data_raw %>% 
+  mutate_at(vars(GEOID), ~ .x %>% as.numeric())
+
+
+
+# 
+# # fix var content
+# clean_columns %>% 
+#   mutate(Cntmnt_Fnd_Other_Metals = case_when(Cntmnt_Fnd_Other_Metals == "x" ~ "Y",
+#                                              TRUE ~ Cntmnt_Fnd_Other_Metals)) %>% 
+#   count(Cntmnt_Fnd_Other_Metals)
+# 
+# fnclean_cntmnt <- function(cntmnt){
+#   mutate(cntmnt = case_when(cntmnt == "x" ~ "Y",
+#                                              TRUE ~ cntmnt))
+# }
+# 
+# clean_columns %>% 
+#   mutate_at(c("Cntmnt_Fnd_Other_Metals", "Cntmnt_Fnd_Petroleum"), fnclean_cntmnt)
+# 
+# clean_columns %>% 
+#   mutate(across(c("Cntmnt_Fnd_Other_Metals", "Cntmnt_Fnd_Petroleum"), fnclean_cntmnt)) %>% 
+#   count(Cntmnt_Fnd_Other_Metals)
+# 
+# clean_columns %>% 
+#   mutate(Cntmnt_Fnd_Other_Metals = case_when(Cntmnt_Fnd_Other_Metals == "x" ~ "Y",
+#                                              TRUE ~ Cntmnt_Fnd_Other_Metals)) %>% 
+#   mutate(Cntmnt_Fnd_Petroleum = case_when(Cntmnt_Fnd_Petroleum == "x" ~ "Y",
+#                                              TRUE ~ Cntmnt_Fnd_Petroleum)) 
+#   
   
