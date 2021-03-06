bc6c94d3352abb7e6e5ce87cfde2c86b0db66a07
Smit Mehta <mehta.smit92@gmail.com>
2021-03-06 15:00:28 -0500

    Update eda_v1.py

--- scripts/eda_smit/eda_v1.py
+++ scripts/eda_smit/eda_v1.py
@@ -13,7 +13,21 @@ from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 
-
+# reading in the raw data
 df = pd.read_csv('data/brownfields_data_with_county_geoid.csv')
+
+
+# cleaning up the column names for easier future use
+def format_colname(colname):
+    
+    colname = colname.replace(' ', '_') #replace spaces with underscores 
+    return colname
+
+
+colnames = list(df.columns)
+newcolnames = list()
+for eachcol in colnames:
+    newcolnames.append(format_colname(eachcol))
+
 
 
