77bfdaa76d1b7ae2a324525fbc7039513fbc0669
Smit Mehta <mehta.smit92@gmail.com>
2021-03-07 11:36:34 -0500

    Create eda_v1.R

---
+++ scripts/eda_smit/eda_v1.R
@@ -0,0 +1,14 @@
+
+#################################
+# EDA v1
+# author: Smit Mehta
+# github: @smit-m
+#################################
+
+# loading libraries
+library(tidyverse)
+
+
+# Loading Data
+df <- read_csv('data/brownfields_data_with_county_geoid.zip')
+
