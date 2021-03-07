50971f10fb223f3fa323d3e382ef119b39600bd2
Lamia <lamiazsz@gmail.com>
2021-03-07 01:37:36 +0100

    A few added keywords

--- scripts/lamiaz/former_use_keyword_extraction.R
+++ scripts/lamiaz/former_use_keyword_extraction.R
@@ -38,6 +38,15 @@ building_word_table = sort(table(building_words),decr=T)
 common_building_word = building_word_table[1:20]
 common_building_word
 
+## Property
+property_words = str_squish(unlist(str_extract_all(dh_plus,"(?i)[a-z/\\-]+(?=\\s*(?:site[\\s\\-])?property)")))
+property_words = property_words[nchar(property_words)>2]
+property_words = property_words[!str_detect(property_words,"one|two|three|four|five|storey|floor|square|foot|feet|^site$")]
+property_word_list = unique(property_words)
+property_word_table = sort(table(property_words),decr=T)
+common_property_word = property_word_table[1:30]
+common_property_word
+
 ## Facility
 facility_words = str_squish(unlist(str_extract_all(dh_plus,"(?i)[a-z/\\-]+(?=\\s*(?:site[\\s\\-])?facility)")))
 facility_words = facility_words[nchar(facility_words)>2]
@@ -105,14 +114,15 @@ properties = properties %>% mutate(commercial_building = str_detect(`Description
 									repair_facility = str_detect(`Description/History`,"(?i)repair[a-z/\\-]*\\s*(?:site\\s+)?facility"),
 									storage_facility_or_warehouse = str_detect(`Description/History`,"(?i)storage[a-z/\\-]*\\s*(?:site\\s+)?(?:facility|yard)|ware[\\s+-]house"),
 									manufacture = str_detect(`Description/History`,"(?i)manufactur"),	
-									store = str_detect(`Description/History`,"(?i)\\bstore|shop"),	
+									residential = str_detect(`Description/History`,"(?i)residential"),	
+									store = str_detect(`Description/History`,"(?i)\\bstore|shop|\\bretail"),	
 									parking = str_detect(`Description/History`,"(?i)parking"),	
 									school = str_detect(`Description/History`,"(?i)school"),
-									industrial = str_detect(`Description/History`,"(?i)industrial"),
-									rail = str_detect(`Description/History`,"(?i)\\brail|train"),
-									vacant_land = str_detect(`Description/History`,"(?i)vacant[a-z/\\-]*\\s*lands?\\b"),
+									industrial = str_detect(`Description/History`,"(?i)industr(?:ial|y)"),
+									rail = str_detect(`Description/History`,"(?i)\\brail|\\btrain"),
+									vacant = str_detect(`Description/History`,"(?i)\\bvacant"),
 									farm_land = str_detect(`Description/History`,"(?i)(?:farm|agricultural)[a-z/\\-]*\\s*lands?\\b"),
-									forest = str_detect(`Description/History`,"(?i)forest|\\btimber\\b|\\bwood|"),
+									forest = str_detect(`Description/History`,"(?i)forest|\\btimber\\b|\\bwood"),
 									wet_land = str_detect(`Description/History`,"(?i)\\bwet\\s+land|\\bswamp"),
 									junk_yard = str_detect(`Description/History`,"(?i)(?:junk|scrap|salvage)[a-z/\\-]*\\s*yard"))
 	
