128574f1e7061c278c0adb5ae93b528487ed3828
Joseph Pope <joseph.pope@successacademies.org>
2021-03-07 10:36:56 -0500

    sunday morning update for lda

--- scripts/joepope44/nlp_desc.R
+++ scripts/joepope44/nlp_desc.R
@@ -19,56 +19,79 @@ geo_text <- geo_data_raw %>%
     coalesce(desc, highlights),
     highlights)
     ) %>% 
-  distinct(text, coop_num)
+  distinct(text, coop_num) %>% 
+  drop_na() %>% 
+  filter(coop_num != "n/a")
 
 head(geo_text, 10)
 
 
 # Create DTM --------------------------------------------------------------
 
+custom_stopwords <- c("site", "property", "use", "former", "building", "used")
+
 acres_corpus = Corpus(VectorSource(geo_text$text))
 
 acres_corpus = tm_map(acres_corpus, content_transformer(tolower))
 acres_corpus = tm_map(acres_corpus, removeNumbers)
 acres_corpus = tm_map(acres_corpus, removePunctuation)
-acres_corpus = tm_map(acres_corpus, removeWords, c("the", "and", stopwords("english")))
+acres_corpus = tm_map(acres_corpus, removeWords, c("the", "and", custom_stopwords, stopwords("english")))
 acres_corpus =  tm_map(acres_corpus, stripWhitespace)
 
 acres_dtm <- DocumentTermMatrix(acres_corpus)
 
-review_dtm = removeSparseTerms(acres_dtm, 0.99)
+review_dtm = removeSparseTerms(acres_dtm, 0.999)
 review_dtm
+
+
+raw.sum <- apply(review_dtm, 1, FUN=sum)
+dtm <- review_dtm[raw.sum != 0, ]
 
 
 # LDA ---------------------------------------------------------------------
 
-custom_stopwords <- c("site", "property", "use", "former", "building")
-
-tidy_docs <- geo_text %>% 
-  select(coop_num, text) %>% 
-  unnest_tokens(output = word, 
-                input = text,
-                stopwords = c(stopwords::stopwords("en"), 
-                              stopwords::stopwords(source = "smart"),
-                              custom_stopwords),
-                token = "ngrams",
-                n_min = 1, n = 2) %>% 
-  count(coop_num, word) %>% 
-  tidytext::
-  filter(n>1) 
-
-d <- tidy_docs %>% 
-  cast_sparse(coop_num, word, n)
-
-
-# create a topic model
-m <- FitLdaModel(dtm = d, 
-                 calc_r2 = TRUE,
-                 k = 10,
-                 iterations = 100,
-                 burnin = 175)
+# tidy_docs <- geo_text %>% 
+#   select(coop_num, text) %>% 
+#   unnest_tokens(output = word, 
+#                 input = text,
+#                 stopwords = c(stopwords::stopwords("en"), 
+#                               stopwords::stopwords(source = "smart"),
+#                               custom_stopwords),
+#                 token = "words") %>% 
+#   count(coop_num, word) %>% 
+#   filter(n>5)
+# 
+# d <- tidy_docs %>% 
+#   cast_sparse(coop_num, word, n)
+# 
+# # create a topic model
+# m <- FitLdaModel(dtm = d, 
+#                  calc_r2 = TRUE,
+#                  k = 4,
+#                  iterations = 10,
+#                  burnin = 5)
 
 
+rm(ac_lda)
+ac_lda <- LDA(dtm, k = 6, control = list(seed = 1234))
+ac_lda
+
+topics <- tidy(ac_lda, matrix = "beta")
+topics
+
+top_terms <- topics %>%
+  group_by(topic) %>%
+  top_n(5, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+top_terms
 
 
+top_terms %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(beta, term, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  scale_y_reordered()
 
+terms(ac_lda, 5)
